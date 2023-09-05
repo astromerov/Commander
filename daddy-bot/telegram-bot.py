c00b0dbe78c803d10f0696d41eca0edfd326b239
Umerov Javokhir <96944796+astromerov@users.noreply.github.com>
2023-09-05 12:23:23 +0500

    chore: code

---
+++ daddy-bot/telegram-bot.py
@@ -0,0 +1,31 @@
+import telebot
+
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+TOKEN = '6484238483:AAHEUF9m6Bx53y2xLWfFUGL6WnJfB6YNyL8'
+
+# Create an instance of the Bot
+bot = telebot.TeleBot(TOKEN)
+
+# Function to handle the /start command
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Hello! You can find more about me at https://www.t.me/astromerov.")
+
+# Function to handle the /help command
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, "This is a simple bot. Use /start to get started and /learn to learn more.")
+
+# Function to handle the /learn command
+@bot.message_handler(commands=['learn'])
+def learn(message):
+    bot.send_message(message.chat.id, "You can learn more about me at https://www.t.me/astromerov.")
+
+# Function to handle the /contact command
+@bot.message_handler(commands=['contact'])
+def contact(message):
+    user_id = message.from_user.id
+    contact_url = f"https://t.me/astromerov?start=chat&user={user_id}"
+
+# Start the bot
+bot.polling()
